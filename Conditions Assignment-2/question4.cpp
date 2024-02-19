57c8eb634fc2c5062a8e42f567a371feeafb62ee
Suman0252 <157882060+Suman0252@users.noreply.github.com>
2024-02-19 19:29:12 +0530

    Add files via upload

---
+++ Conditions Assignment-2/question4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int r,h;
+    cout<<"Enter the radius and height of cylinder"<<endl;
+    cin>>r;
+    cin>>h;
+    int pi=3.14;
+    int volume=pi*r*r*h;
+    cout<<volume;
+ 
+
+
+
+}
